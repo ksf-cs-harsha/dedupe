d020f5203f612a81f70f39abc79e63856746d40c
ksf-cs-harsha <157358899+ksf-cs-harsha@users.noreply.github.com>
2024-04-29 15:39:51 +0530

    uni dedupe
    The code has be altered

---
+++ dedupe 2/src/main/java/Dedupe/dedupe/DedupeApplication.java
@@ -0,0 +1,13 @@
+package Dedupe.dedupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedupeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DedupeApplication.class, args);
+	}
+
+}
---
+++ dedupe 2/src/test/java/Dedupe/dedupe/DedupeApplicationTests.java
@@ -0,0 +1,13 @@
+package Dedupe.dedupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedupeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
